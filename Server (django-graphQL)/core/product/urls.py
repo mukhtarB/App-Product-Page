eca5b03282aa227837e0ab7da21d8b40ff03f917
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-16 18:18:11 +0100

    setup product app

--- Server (django-graphQL)/core/product/urls.py
+++ Server (django-graphQL)/core/product/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('product.urls')),
+    path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
