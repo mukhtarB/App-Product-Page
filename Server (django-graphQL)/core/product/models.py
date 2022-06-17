f6acc05ea970774bbb511985e73e0e08f7b51700
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-17 03:24:32 +0100

    bug fix: fix data type for db column; name

--- Server (django-graphQL)/core/product/models.py
+++ Server (django-graphQL)/core/product/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.URLField(blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
     stock_id = models.CharField(max_length=100, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
