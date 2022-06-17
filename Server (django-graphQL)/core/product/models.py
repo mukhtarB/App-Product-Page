47028e48949f4ca4457575d759dbbf2206ee2c7b
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-17 02:41:12 +0100

    set up: db models

--- Server (django-graphQL)/core/product/models.py
+++ Server (django-graphQL)/core/product/models.py
@@ -1,3 +1,14 @@
+from email.mime import image
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.URLField(blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    stock_id = models.CharField(max_length=100, blank=False, null=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+    image = models.URLField(blank=False, null=False)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
