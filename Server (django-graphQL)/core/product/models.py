9df6f313793121f2aaad76ebb2fca0b4dec52253
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-17 02:56:13 +0100

    update product model

--- Server (django-graphQL)/core/product/models.py
+++ Server (django-graphQL)/core/product/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     stock_id = models.CharField(max_length=100, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
     image = models.URLField(blank=False, null=False)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
 
     def __str__(self) -> str:
         return f'{self.name}'
