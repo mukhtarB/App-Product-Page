26641c7a89cd5a8a06af1d692bbe4127a1fb624c
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-18 20:03:09 +0100

    implement apollo client

--- Client (React)/core/src/index.js
+++ Client (React)/core/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './Components/App';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { endpoint_URI } from '../config';
+
+const client = new ApolloClient({
+    uri: endpoint_URI,
+    cache: new InMemoryCache(),
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    <ApolloProvider client={client}>
         <App/>
-    </React.StrictMode>
+    </ApolloProvider>
 );
