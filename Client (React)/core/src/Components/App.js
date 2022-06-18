d1ab79b40b3ccaecf8f264d64d0162d94a05c195
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-18 19:01:13 +0100

    Setup Layout

--- Client (React)/core/src/Components/App.js
+++ Client (React)/core/src/Components/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import logo from '../logo.svg';
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="App container">
+
+            <h2> Product Page: Item List</h2>
+
+            <div className='product-list'>
+
+                <div className='product-item'>
+                    <div className='img-wrapper'>
+                        <img src={logo} alt='Not an image yet' title=''/>
+                    </div>
+
+                    <p>Name: Name of Product</p>
+                    <p>Price: Product Price</p>
+                    <button className='btn btn-primary'>add to cart</button>
+                </div>
+
+                <div className='product-item'>
+                    <div className='img-wrapper'>
+                        <img src={logo} alt='Not an image yet' title=''/>
+                    </div>
+
+                    <p>Name: Name of Product</p>
+                    <p>Price: Product Price</p>
+                    <button className='btn btn-primary'>add to cart</button>
+                </div>
+
+                <div className='product-item'>
+                    <div className='img-wrapper'>
+                        <img src={logo} alt='Not an image yet' title=''/>
+                    </div>
+
+                    <p>Name: Name of Product</p>
+                    <p>Price: Product Price</p>
+                    <button className='btn btn-primary'>add to cart</button>
+                </div>
+
+            </div>
         </div>
     );
 }
