73dc3d8779edf3d320aa8f1737fb608ce73b3196
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-19 12:34:58 +0100

    rename config file

--- Client (React)/core/src/constants.js
+++ Client (React)/core/src/constants.js
@@ -1,4 +1,4 @@
 // Isolating / modularizing constant application data
-const endpoint_URI = 'http://localhost:4000/graphql';
+const endpoint_URI = 'http://localhost:8000/graphql';
 
 export { endpoint_URI };
